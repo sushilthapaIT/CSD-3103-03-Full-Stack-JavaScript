d540251929d33a6fa735728fa91150acf126fb8c
Sushil Thapa <87489909+sushilthapaIT@users.noreply.github.com>
2024-10-01 11:17:37 -0400

    update

--- Week 3/prac/routes/index.js
+++ Week 3/prac/routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/contact', (res,req) => {
+  res.send("Hello You can contact us...")
+})
+
 module.exports = router;
